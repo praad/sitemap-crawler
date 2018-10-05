5b107dec0bccb046f55e763f92e2d627fdf4b797
Adam Prancz <adam.prancz@gmail.com>
2018-10-06 01:35:19 +0200

    It helps to avoid a fatal error in bad sitemaps
    PHP Fatal error:  Uncaught Error: Call to a member function find() on boolean in

--- src/Alc/SitemapCrawler.php
+++ src/Alc/SitemapCrawler.php
@@ -39,43 +39,39 @@ class SitemapCrawler {
 		$helper = new HtmlDomParserHelper();
 		$parser = $helper->parse($url);
 
-		$nodes = $parser->find('sitemap loc');
+       		if ($parser) {
+		    $nodes = $parser->find('sitemap loc');
 
-		if( !empty($nodes) ) {
-
-			foreach( $nodes as $node ) {
-
-				$this->sitemaps[] = $node->innertext;
+		    if (!empty($nodes)) {
+			foreach ($nodes as $node) {
+			    $this->sitemaps[] = $node->innertext;
 			}
-		}
+		    }
 
-		$nodes = $parser->find('url');
+		    $nodes = $parser->find('url');
 
-		if( !empty($nodes) ) {
+		    if (!empty($nodes)) {
+			foreach ($nodes as $node) {
+			    $loc = $node->find('loc', 0);
 
-			foreach( $nodes as $node ) {
+			    if (!isset($this->urls[$loc->innertext])) {
+				$url = new SitemapUrl();
 
-				$loc = $node->find('loc', 0);
+				// Set url
+				$url->setUrl($loc->innertext);
 
-				if( !isset($this->urls[$loc->innertext]) ) {
+				// Find alternate links
+				$alternateLinks = $node->find('xhtml:link[rel=alternate]');
 
-					$url = new SitemapUrl();
-
-					// Set url
-					$url->setUrl($loc->innertext);
-
-					// Find alternate links
-					$alternateLinks = $node->find('xhtml:link[rel=alternate]');
-
-					foreach($alternateLinks as $alternateLink) {
-
-						$url->addAlternateLinks($alternateLink->getAttribute('hreflang'), $alternateLink->getAttribute('href'));
-					}
-
-					// Push in array
-					$this->urls[$url->getUrl()] = $url;
+				foreach ($alternateLinks as $alternateLink) {
+				    $url->addAlternateLinks($alternateLink->getAttribute('hreflang'), $alternateLink->getAttribute('href'));
 				}
+
+				// Push in array
+				$this->urls[$url->getUrl()] = $url;
+			    }
 			}
+		    }
 		}
 
 		$helper->clear();
